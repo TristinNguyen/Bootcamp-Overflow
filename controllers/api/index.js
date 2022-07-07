5dcfa9931897e79077982ebc4b801aaa2fc94eb1
Victor Nunez <VN135766@gmail.com>
2022-07-07 13:50:22 -0400

    added homepage function

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-// const postRoutes = require('./post-routes');
+//const questionRoutes = require('./question-routes');
 // const commentRoutes = require ('./comment-routes');
 
 router.use('/users', userRoutes);
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
+//this collects and packages up all the API routes
 const router = require('express').Router();
+const homeRoutes = require('./home-routes.js');
+
 
 const apiRoutes = require('./api');
-
+//add API prefixes
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
