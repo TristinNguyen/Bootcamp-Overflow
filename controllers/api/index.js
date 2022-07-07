79915e44bbc43d314031f5651ecec86399e1931c
Victor Nunez <VN135766@gmail.com>
2022-07-07 13:59:54 -0400

    added comments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-//const questionRoutes = require('./question-routes');
-// const commentRoutes = require ('./comment-routes');
+const questionRoutes = require('./question-routes');
+//const commentRoutes = require ('./comment-routes');
 
+// add api prefixes
 router.use('/users', userRoutes);
-// router.use('/posts', postRoutes);
+router.use('/questions', questionRoutes);
 // router.use('/comments', commentRoutes);
 
 module.exports = router;
