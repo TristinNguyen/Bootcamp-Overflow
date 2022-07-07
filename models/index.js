601e9419e1741403cbeb12fbd521e9e5f5a8cfe9
rstanbaugh <rich_bootcamp@icloud.com>
2022-07-06 21:55:29 -0400

    added model and basic route for Question

--- models/index.js
+++ models/index.js
@@ -1,9 +1,18 @@
 // import all models
-// const Post = require('./Post');
+const Question = require('./Question');
 const User = require('./User');
 // const Vote = require('./Vote');
 // const Comment = require('./Comment');
 
 // create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
-module.exports = { User };
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+
+// exports
+module.exports = { User, Question };
