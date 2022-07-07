a5c8ee236cb643ddfb99061a92d03002964882eb
rstanbaugh <rich_bootcamp@icloud.com>
2022-07-07 14:20:06 -0400

    starter code for user / question seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,17 @@
-INSERT INTO users (id, username, email, password) 
+USE bootcamp_post_db;
+
+INSERT INTO user (id, username, email, password) 
 VALUES
-  (1, 'Ronald', 'ronnym#gmail.com', 'password123'),
+  (1, 'Ronald', 'ronnym@gmail.com', 'password123'),
   (2, 'Joey Doaks', 'jd@aim.com', 'smokingfool'),
   (3, 'FunnyGuy', 'verry-funny@jokes.com', 'hahahaha'),
   (4, 'Humpty Dumpty', 'HD@aol.com', 'Oh-Whatta-Fall'),
   (5, 'Cartwright', 'fuck@you.com', 'YourParentsAreInTheChilli'),
-  (6, 'Bart Simpson', 'You@suck.com', 'IHeartCartman')
+  (6, 'Bart Simpson', 'You@suck.com', 'IHeartCartman');
 
 
-INSERT INTO questions (title, question, user_id) 
+INSERT INTO question (title, question, user_id) 
 VALUES  
   ('Why Me?', 'What is the meaning of life?', 1),
   ('Chilli', 'What is the best way to cook someones parents?', 5),
-  ('what time is it?', 'Now is the time for all good men to come to the aid of their country', 2)
+  ('what time is it?', 'Now is the time for all good men to come to the aid of their country', 2);
