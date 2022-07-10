de20b35fed72aba02e155ee14c4ea845f5a8c895
Victor Nunez <VN135766@gmail.com>
2022-07-10 09:15:44 -0400

    finally got dashboard to work

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,99 +1,87 @@
-const router = require("express").Router();
-const sequelize = require("../config/connection");
-// this requires user to be logged in
-const withAuth = require("../utils/auth");
-const { Question, User, Answer } = require("../models");
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+// req user to be logged in
+const withAuth = require('../utils/auth');
+const { question, User, answer } = require('../models');
 
-//this GETS dashboard page
-router.get("/", withAuth, (req, res) => {
-  Question.findAll({
-    where: {
-      // now only show posts on user dashboard that have been created by the user
-      // and use the ID from the session
-      user_id: req.session.user_id,
-    },
-    attributes: [
-      "id",
-      "question",
-      "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE question.id = vote.question)"
-        ),
-        "vote_count",
+// GET dashboard page
+router.get('/', withAuth, (req, res) => {
+  console.log(req.query);
+
+    question.findAll({
+      attributes: [
+        'id',
+        'question',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE question.id = vote.question_id)'), 'vote_count']
       ],
-    ],
-    include: [
-      {
-        model: Answer,
-        attributes: ["id", "answer_text", "question", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
+      include: [
+        {
+          model: answer,
+          attributes: ['id', 'answer_text', 'question_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
         },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbQuestionData) => {
-      // serialize data before passing to template
-      const posts = dbQuestionData.map((question) => question.get({ plain: true }));
-      res.render("dashboard", { questions, loggedIn: true });
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+    .then(dbquestionData => {
+    // serialize data before passing to template
+    const questions = dbquestionData.map(question => question.get({ plain: true }));
+    console.log(questions)
+    res.render('dashboard', { questions, loggedIn: true });
+    })
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
     });
 });
 
-router.get("/edit/:id", withAuth, (req, res) => {
-  Post.findByPk(req.params.id, {
-    attributes: [
-      "id",
-      "question",
-      "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE question.id = vote.question)"
-        ),
-        "vote_count",
+router.get('/edit/:id', withAuth, (req, res) => {
+    question.findByPk(req.params.id, {
+      attributes: [
+        'id',
+        'question_content',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE question.id = vote.question_id)'), 'vote_count']
       ],
-    ],
-    include: [
-      {
-        model: Answer,
-        attributes: ["id", "answer_text", "question", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
+      include: [
+        {
+          model: answer,
+          attributes: ['id', 'answer_text', 'question_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
         },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbQuestionData) => {
-      if (dbQuestionData) {
-        const post = dbQuestionData.get({ plain: true });
-
-        res.render("edit-question", {
-          post,
-          loggedIn: true,
-        });
-      } else {
-        res.status(404).end();
-      }
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
     })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
+      .then(dbquestionData => {
+        if (dbquestionData) {
+          const question = dbquestionData.get({ plain: true });
+          
+          res.render('edit-question', {
+            question,
+            loggedIn: true
+          });
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
 });
 
 module.exports = router;
