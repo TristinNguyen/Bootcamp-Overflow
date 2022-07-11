c1eb70d4027601236417718f16f5b03a6c8d78b5
Victor Nunez <VN135766@gmail.com>
2022-07-11 18:34:24 -0400

    commit

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,15 +1,17 @@
 async function logout() {
-    console.log("please work")
-  const response = await fetch ("/api/users/logout",{
-    method: "post",
-    headers: { "Content-Type": "application/json" },
-  });
+    const response = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
 
-  if (response.ok) {
-    document.location.replace("/");
-  } else {
-    alert(response.statusText);
-  }
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
 }
 
-document.querySelector("#logout").addEventListener("click", logout);
+
+document.querySelector('#logout').addEventListener('click', logout);
