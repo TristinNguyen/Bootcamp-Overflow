e703387f6bc34658e0b69e19a41a9eb2a93f2150
Victor Nunez <VN135766@gmail.com>
2022-07-13 14:48:11 -0400

    new question working

--- public/javascript/add-question.js
+++ public/javascript/add-question.js
@@ -33,7 +33,7 @@ async function newQuestionHandler(event) {
   }
 }
 
-document.getElementById('addQuestion').addEventListener('click', btn.onclick);
+// document.getElementById('new-question-form').addEventListener('click', btn.onclick);
 
 // document.querySelector('.newquestion').addEventListener('click', btn.onclick);
 document.querySelector('.new-question-form').addEventListener('submit', newQuestionHandler);
